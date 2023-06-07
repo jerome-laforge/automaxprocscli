63b2a5054477775bf6f9217d5a000042d339feb9
Jérôme Laforge <jerome.laforge@orange.com>
2023-06-07 14:25:31 +0200

    simplify

--- main.go
+++ main.go
@@ -1,43 +1,36 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
-    "os"
-    "runtime"
-    "go.uber.org/automaxprocs/maxprocs"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"go.uber.org/automaxprocs/maxprocs"
 )
 
 func main() {
-    v := flag.Bool("v", false, "display GOMAXPROCS before and after or if no change has occurred")
-    flag.Parse()
+	v := flag.Bool("v", false, "display GOMAXPROCS before and after or if no change has occurred")
+	flag.Parse()
 
-    if *v {
-       verbose()
-    }
+	before := runtime.GOMAXPROCS(0)
 
-    if _, err := maxprocs.Set(); err != nil {
-       log.Fatal(err)
-    }
+	if _, err := maxprocs.Set(); err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Print(runtime.GOMAXPROCS(0))
-}
+	after := runtime.GOMAXPROCS(0)
 
-func verbose() {
-    before := runtime.GOMAXPROCS(0)
+	if *v {
+		if before == after {
+			fmt.Printf("GOMAXPROCS: %v (no change)\n", before)
+		} else {
+			fmt.Printf("GOMAXPROCS before: %v after: %v\n", before, after)
+		}
 
-    if _, err := maxprocs.Set(); err != nil {
-       log.Fatal(err)
-    }
+		os.Exit(0)
+	}
 
-    after := runtime.GOMAXPROCS(0)
-
-    if before == after {
-       fmt.Printf("GOMAXPROCS: %v (no change)\n", before)
-       os.Exit(0)
-    }
-
-    fmt.Printf("GOMAXPROCS before: %v after: %v\n", before, after)
-    os.Exit(0)
+	fmt.Print(after)
 }
